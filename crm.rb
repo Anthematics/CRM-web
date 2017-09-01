5f2340427409f7fcc853d1d6f8017ef329950493
Anthematics <jasonharder2@gmail.com>
2017-09-01 16:36:39 -0400

    v 0.3 >Adding a contact to the crm now works 100%

--- crm.rb
+++ crm.rb
@@ -13,6 +13,10 @@ require 'sinatra'
 		 erb :index
 	end
 
+	get '/about' do
+		erb :about
+	end
+
 	get '/contacts' do
 				@collection = Contact.all
 				erb :contacts
@@ -30,11 +34,25 @@ require 'sinatra'
 			 else
 				 raise Sinatra::NotFound
 		 end
+
+	get '/contacts/:id/edit' do
+		erb:edit_contact
 	end
 
-	get '/about' do
-		erb :about
+
+	# =============POST ROUTES HERE ====
+
+	post '/contacts' do
+		Contact.create(
+			first_name:params[:first_name],
+			last_name: params[:last_name],
+			email:     params[:email],
+			note:      params[:note]
+		)
+		redirect to ('/contacts')
 	end
+
+
 
 	after do
 		ActiveRecord::Base.connection.close
